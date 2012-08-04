e915d8ea09ddfb921e5d2de1777b1691d6cac6bb
Denis Buka <Denis Buka@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-04 13:46:36 +0000

    git-svn-id: https://plugins.svn.wordpress.org/drop-in-dropbox/tags/0.2.7@581711 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ run2.php
@@ -0,0 +1,1 @@
+<?phprequire_once( '../../../wp-load.php' );require_once( dirname(__FILE__) . '/functions.php' );require_once( dirname(__FILE__) . '/DropboxUploader.php' );require_once( dirname(__FILE__) . '/a.charset.php' );if( isset( $_POST["count"] ) ) { 	$count = $_POST["count"];	dropNow( $count, 'run1' );}?>
